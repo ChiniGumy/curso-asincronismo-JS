68e1619b533a602bee29582a2e0cd7f2a870ba84
David Sánchez Romero <dsebastiansr@hotmail.com>
2023-02-19 17:20:30 -0500

    [CLASS: Fetch Data]

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,4 @@
-const XMLHttpRequest = requerie('xmlhttpequest');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const API = 'https://api.escuelajs.co/api/v1/';
 
 
@@ -12,11 +12,33 @@ function fetchData(urlAPI, callback) {
 		if (xhttp.readyState === 4) {								// 4 = XMLHttpRequest completado
 			if (xhttp.status === 200) {								// 200 = La solicitud ha correcta
 				callback(null, JSON.parse(xhttp.responseText));
-			}
-		} else {
-			const error = new Error('Error' + urlAPI);
-			return callback(error, null);
-		}
-	}
+			} else {
+				const error = new Error('Error' + urlAPI);
+				return callback(error, null);
+			};
+		};
+	};
 	xhttp.send();
 }
+
+
+// Fetch Data =====================================================================================
+
+fetchData(`${API}/products`, function (error1, data1) {
+
+	if (error1) return console.error(error1);
+
+	fetchData(`${API}/products/${data1[0].id}`, function(error2, data2){
+	
+		if (error2) return console.error(error2);
+	
+		fetchData(`${API}/products/${data2?.category?.id}`, function(error3, data3){
+			
+			if (error3) return console.error(error3);
+			console.log(data1[0]);
+			console.log(data2.title);
+			console.log(data3.name);
+
+		});
+	});
+});
